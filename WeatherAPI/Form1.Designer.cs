54c255c7ed97c9e768d7cc98f56082192f295372
Janki Joshi <joshijankii3010@gmail.com>
2018-11-24 22:30:04 -0500

    Weather API uploaded

---
+++ WeatherAPI/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace WeatherAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_cityName = new System.Windows.Forms.Label();
+            this.lbl_country = new System.Windows.Forms.Label();
+            this.lbl_Temp = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_cityName
+            // 
+            this.lbl_cityName.AutoSize = true;
+            this.lbl_cityName.Location = new System.Drawing.Point(92, 96);
+            this.lbl_cityName.Name = "lbl_cityName";
+            this.lbl_cityName.Size = new System.Drawing.Size(50, 13);
+            this.lbl_cityName.TabIndex = 0;
+            this.lbl_cityName.Text = "Cityname";
+            // 
+            // lbl_country
+            // 
+            this.lbl_country.AutoSize = true;
+            this.lbl_country.Location = new System.Drawing.Point(92, 123);
+            this.lbl_country.Name = "lbl_country";
+            this.lbl_country.Size = new System.Drawing.Size(35, 13);
+            this.lbl_country.TabIndex = 1;
+            this.lbl_country.Text = "label1";
+            // 
+            // lbl_Temp
+            // 
+            this.lbl_Temp.AutoSize = true;
+            this.lbl_Temp.Location = new System.Drawing.Point(92, 147);
+            this.lbl_Temp.Name = "lbl_Temp";
+            this.lbl_Temp.Size = new System.Drawing.Size(35, 13);
+            this.lbl_Temp.TabIndex = 2;
+            this.lbl_Temp.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "City: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Country: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Temperature: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Enter City Name: ";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(108, 30);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 7;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(224, 28);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // button1
+            // 
+            this.button1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button1.Location = new System.Drawing.Point(16, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(295, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_Temp);
+            this.Controls.Add(this.lbl_country);
+            this.Controls.Add(this.lbl_cityName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_cityName;
+        private System.Windows.Forms.Label lbl_country;
+        private System.Windows.Forms.Label lbl_Temp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WeatherAPI/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using TemperatorConvertService;
+
+namespace WeatherAPI
+{
+    public partial class Form1 : Form
+    {
+        const string APPID = "745eaa220b2a71c1d3bedba10a6e0064";
+        string cityName = "Kitchener";
+        string currentUnit = "";
+        const string CELSIUS = "Celsius";
+        const string FAHRENHEIT = "Fahrenheit";
+        WeatherInfo.root outPut;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            label1.Hide();
+            label2.Hide();
+            label3.Hide();
+            lbl_cityName.Hide();
+            lbl_country.Hide();
+            lbl_Temp.Hide();
+            button1.Hide();
+            
+        }
+
+        void getWeather(string city)
+        {
+
+            using (WebClient web = new WebClient())
+            {
+                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0},CA&appid={1}&units=metric&cnt=6", city, APPID);
+                try
+                {
+                    var json = web.DownloadString(url);
+
+                    var result = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
+
+                    outPut = result;
+
+                    lbl_cityName.Text = string.Format("{0}", outPut.name);
+                    lbl_country.Text = string.Format("{0}", outPut.sys.country);
+                    lbl_Temp.Text = string.Format("{0} \u00B0" + "C", outPut.main.temp);
+                    button1.Text = "Convert to " + FAHRENHEIT;
+                    currentUnit = CELSIUS;
+                    showDetails();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("City name does not exist in Canada");
+                }
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var cityInput = txtCity.Text;
+            getWeather(cityInput);
+        }
+
+        private void showDetails()
+        {
+            label1.Show();
+            label2.Show();
+            label3.Show();
+            lbl_cityName.Show();
+            lbl_country.Show();
+            lbl_Temp.Show();
+            button1.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Service1 s = new Service1();
+
+            if (currentUnit != FAHRENHEIT)
+            {
+                button1.Text = "Convert to " + CELSIUS;
+                lbl_Temp.Text = string.Format("{0} \u00B0" + "F", s.ToFahrenheit(outPut.main.temp));
+                currentUnit = FAHRENHEIT;
+            } else
+            {
+                button1.Text = "Convert to " + FAHRENHEIT;
+                lbl_Temp.Text = string.Format("{0} \u00B0" + "C", outPut.main.temp);
+                currentUnit = CELSIUS;
+            }
+            
+        }
+    }
+}
